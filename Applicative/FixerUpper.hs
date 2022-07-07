2d93a6d802756cfe4af3619e8f132545223f1ab4
dingshimin2 <dingshimin2@tuniu.com>
2022-07-07 17:20:50 +0800

    Change Project Structure

--- Applicative/FixerUpper.hs
+++
@@ -1,9 +0,0 @@
-module Applicative.FixerUpper where
-
--- 1.
-
-x = const <$> Just "Hello" <*> pure "World"
-
--- 2.
-
-y = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
--- Testing/morse/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for morse
-
-## Unreleased changes
