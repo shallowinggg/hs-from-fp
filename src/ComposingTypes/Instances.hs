8f66628f0a5a10c8d6f2be82ff035333595bdc47
shallowinggg <shallowinggg@icloud.com>
2022-08-02 21:24:55 +0800

    Add ComposingTypes

---
+++ src/ComposingTypes/Instances.hs
@@ -0,0 +1,13 @@
+module ComposingTypes.Instances where
+
+newtype Compose f g a = Compose {getCompose :: f (g a)}
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap f (Compose fga) = (foldMap . foldMap) f fga
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse f (Compose fga) = Compose <$> (traverse . traverse) f fga
